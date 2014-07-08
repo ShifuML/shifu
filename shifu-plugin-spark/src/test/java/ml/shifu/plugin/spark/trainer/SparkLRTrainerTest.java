0c555cdd3dea121f033096cd0904eee16e6ae1a3
lisahua[user] <lisahua46@gmail.com>
2014-07-08 11:38:10 -0700

    exclude akka in shifu-core in shifu-plugin-spark due to the version conflict.

--- shifu-plugin-spark/src/test/java/ml/shifu/plugin/spark/trainer/SparkLRTrainerTest.java
+++ shifu-plugin-spark/src/test/java/ml/shifu/plugin/spark/trainer/SparkLRTrainerTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 public class SparkLRTrainerTest {
 
-    //@Test
+    @Test
     public void testSparkLRTrainer() throws Exception {
         ShifuRequest req = JSONUtils.readValue(new File("src/test/resources/trainer/sparkLR/SparkLRExecTrainRequest.json"), ShifuRequest.class);
         RequestDispatcher.dispatch(req);
