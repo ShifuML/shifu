e1f10010e126c0d25ff1d191f61689e4f025c327
raoulcjohnson <raouljohnson@paypal.com>
2014-08-08 11:56:10 -0700

    Changed to load data either strictly locally or strictly HDFS

--- shifu-plugin-pig/src/main/java/ml/shifu/plugin/pig/PigModelExecRequestProcessor.java
+++ shifu-plugin-pig/src/main/java/ml/shifu/plugin/pig/PigModelExecRequestProcessor.java
@@ -1,20 +1,27 @@
 package ml.shifu.plugin.pig;
 
-
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
 import ml.shifu.core.di.spi.RequestProcessor;
 import ml.shifu.core.request.Request;
 import ml.shifu.core.util.LocalDataUtils;
 import ml.shifu.core.util.Params;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.pig.ExecType;
 import org.apache.pig.PigServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,59 +29,125 @@ import java.util.Scanner;
 
 public class PigModelExecRequestProcessor implements RequestProcessor {
 
-    private static Logger log = LoggerFactory.getLogger(PigModelExecRequestProcessor.class);
+	private static Logger log = LoggerFactory
+			.getLogger(PigModelExecRequestProcessor.class);
+	private Configuration conf;
+	private FileSystem fs;
 
-    public void exec(Request req) throws Exception {
+	public void exec(Request req) throws Exception {
 
-        Params params = req.getProcessor().getParams();
+		Params params = req.getProcessor().getParams();
 
-        Boolean localMode = (Boolean) params.get("localMode", false);
+		Boolean localMode = (Boolean) params.get("localMode", false);
 
-        PigServer pigServer;
+		Map<String, String> pigParams = null;
+		PigServer pigServer;
+		
+		if (localMode) {
+			pigServer = new PigServer(ExecType.LOCAL);
+			pigParams = execLocal(req);
+		}
+		else {
+			pigServer = new PigServer(ExecType.MAPREDUCE);
+			pigParams = execHDFS(req);
+		}
+		
+		String pigScriptLocation = "src/main/pig/modelexec.pig";
+		File pigScript = new File(pigScriptLocation);
+		if (!pigScript.exists()) {
+			pigScriptLocation = System.getenv().get("SHIFU_HOME")
+					+ "/plugin/modelexec.pig";
+			pigScript = new File(pigScriptLocation);
+			if (!pigScript.exists())
+				throw new Exception("Could not load modelexec.pig");
+		}
+		log.info("Loading pig script from: " + pigScriptLocation);
+		pigServer.registerScript(pigScriptLocation, pigParams);
 
-        if (localMode) {
-            pigServer = new PigServer(ExecType.LOCAL);
-        } else {
-            pigServer = new PigServer(ExecType.MAPREDUCE);
+
+	}
+
+	
+	private Map<String, String> execLocal(Request req) throws Exception {
+
+		Params params = req.getProcessor().getParams();
+
+		Map<String, String> pigParams = new HashMap<String, String>();
+
+		String[] keys = { "delimiter", "pathData", "pathPMML", "pathResult" };
+
+		for (String key : keys) {
+			pigParams.put(key, params.get(key).toString());
+			log.info(key + " : " + params.get(key).toString());
+		}
+
+		File folderExisting = new File((String) params.get("pathResult"));
+		if (folderExisting.exists()) {
+			FileUtils.deleteDirectory(folderExisting);
+			log.info("Deleting: " + folderExisting.getPath());
+		} else
+			log.info("pathResults does not already exist.");
+
+		String pathHeader = params.get("pathHeader").toString();
+		String headerDelimiter = params.get("headerDelimiter").toString();
+		List<String> header = LocalDataUtils.loadHeader(pathHeader,
+				headerDelimiter);
+
+		pigParams.put("headerString", Joiner.on(',').join(header));
+
+		return pigParams;
+	}
+
+	
+	private Map<String, String> execHDFS(Request req) throws Exception {
+
+		Params params = req.getProcessor().getParams();
+
+		conf = new Configuration();
+		fs = FileSystem.get(conf);
+
+		Map<String, String> pigParams = new HashMap<String, String>();
+
+		String[] keys = { "delimiter", "pathData", "pathPMML", "pathResult" };
+
+		for (String key : keys) {
+			pigParams.put(key, params.get(key).toString());
+			log.info(key + " : " + params.get(key).toString());
+		}
+
+		Path pathResults = new Path((String) params.get("pathResults"));
+		if (!fs.exists(pathResults)) {
+			log.info("Deleting: " + pathResults.getName());
+			fs.delete(pathResults, true);
+		} else
+			log.info("pathResults does not already exist.");
+
+		
+		String headerDelimiter = params.get("headerDelimiter").toString();
+		Path headerFile = new Path((String) params.get("pathHeader"));
+		
+		List<String> header = readHDFSHeader(headerFile,
+				headerDelimiter);
+
+		pigParams.put("headerString", Joiner.on(',').join(header));
+		
+		return pigParams;
+	}
+	
+	private List<String> readHDFSHeader( Path headerFile, String delimiter) throws Exception {
+		
+		List<String> headerList = new ArrayList<String>();
+		
+		FSDataInputStream in = fs.open(headerFile);
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(in)));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            for (String s : Splitter.on(delimiter).split(line)) {
+                headerList.add(s);
+            }
         }
-
-        Map<String, String> pigParams = new HashMap<String, String>();
-
-        String[] keys = {"delimiter", "pathData", "pathPMML", "pathResult"};
-
-        for (String key : keys) {
-            pigParams.put(key, params.get(key).toString());
-            log.info(key + " : " + params.get(key).toString());
-        }
-
-        File folderExisting = new File((String)params.get("pathResult"));  
-        if (folderExisting.exists()){  
-        	//System.out.print("Overwrite pathResult ("+ folderExisting.getPath() + ")? (Y/n): ");
-        	// Scanner in = new Scanner(System.in);
-            //String answer = in.next();
-            //if(!answer.toLowerCase().contains("n"))
-            FileUtils.deleteDirectory(folderExisting);
-            //in.close();
-         	log.info("Deleting: "+folderExisting.getPath());
-       }
-        else log.info("pathResults does not already exist.");
-
-        String pathHeader = params.get("pathHeader").toString();
-        String headerDelimiter = params.get("headerDelimiter").toString();
-        List<String> header = LocalDataUtils.loadHeader(pathHeader, headerDelimiter);
-
-        pigParams.put("headerString", Joiner.on(',').join(header));
-
-        String pigScriptLocation = "src/main/pig/modelexec.pig"; 
-        File pigScript = new File(pigScriptLocation);
-        if(!pigScript.exists()) { 
-        	pigScriptLocation = System.getenv().get("SHIFU_HOME")+"/plugin/modelexec.pig";
-        	pigScript = new File(pigScriptLocation);
-        	if(!pigScript.exists())
-        		throw new Exception("Could not load modelexec.pig");
-        }
-        log.info("Loading pig script from: "+pigScriptLocation);
-        pigServer.registerScript(pigScriptLocation, pigParams);
-        
-    }
+        in.close();
+        scanner.close();
+		return headerList;
+	}
 }
