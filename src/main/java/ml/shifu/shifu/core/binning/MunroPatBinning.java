da9b023b39db2d33e1462846d4ac92f5d2e19603
pengshan.zhang@gmail.com <pengzhang@paypal.com>
2014-12-30 13:27:24 +0800

    1. change Stats.pig to two folders to avoid hdp2.4 replicated join not supported 2. add license files for munropat related code

--- src/main/java/ml/shifu/shifu/core/binning/MunroPatBinning.java
+++ src/main/java/ml/shifu/shifu/core/binning/MunroPatBinning.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ml.shifu.shifu.core.binning;
 
 import java.util.ArrayList;
@@ -10,76 +27,74 @@ import ml.shifu.shifu.core.MunroPatEstimator;
 
 public class MunroPatBinning extends AbstractBinning<Double> {
 
-	private MunroPatEstimator<Double> estimator = null;
-	
-	public MunroPatBinning(int binningNum, List<String> missingValList) {
-		super(binningNum, missingValList);
-		estimator = new MunroPatEstimator<Double>(binningNum);
-	}
+    private MunroPatEstimator<Double> estimator = null;
 
-	public MunroPatBinning(int binningNum) {
-		this(binningNum, null);
-	}
+    public MunroPatBinning(int binningNum, List<String> missingValList) {
+        super(binningNum, missingValList);
+        estimator = new MunroPatEstimator<Double>(binningNum);
+    }
 
-	@Override
-	public void addData(String val) {
-		String fval = StringUtils.trimToEmpty(val);
-		try {
-        	Double dval = Double.valueOf(0.0);
-            dval = Double.parseDouble(fval);
+    public MunroPatBinning(int binningNum) {
+        this(binningNum, null);
+    }
+
+    @Override
+    public void addData(String val) {
+        String fval = StringUtils.trimToEmpty(val);
+        try {
+            Double dval = Double.parseDouble(fval);
             estimator.add(dval);
-            
         } catch (NumberFormatException e) {
             super.incInvalidValCnt();
         }
-	}
-	
-	/**
-	 * set min/max, merge same bins
-	 * 
-	 * @param bins
-	 * @return
-	 */
-	private List<Double> binMerge(List<Double> bins){
-		
-		List<Double> newBins = new ArrayList<Double>();
-		if (bins.size() == 0) {
-			bins.add(Double.NaN);
-			return bins;
-		}
-		
-		Double cur = bins.get(0);
-		newBins.add(cur);
-		
-		int i = 1;
-		while (i < bins.size()) {
-			if (Math.abs(cur - bins.get(i)) > 1e-10) {
-				newBins.add(bins.get(i));
-			}
-			cur = bins.get(i);
-			i ++;
-		}
-		
-		if (newBins.size() == 1) {
-			//special case since there is only 1 candidate in the bins
-			double val = newBins.get(0);
-			newBins = Arrays.asList(new Double[] {Double.NEGATIVE_INFINITY, val});
-		} else if (newBins.size() == 2) {
-			newBins.set(0, Double.NEGATIVE_INFINITY);
-		} else {
-			newBins.set(0, Double.NEGATIVE_INFINITY);
-			//remove the max, and became open interval
-			newBins.remove(newBins.size() - 1);
-		}
-		return newBins;		
-	}
+    }
 
-	@Override
-	public List<Double> getDataBin() {
-		return binMerge(estimator.getQuantiles());
-	}
+    /**
+     * set min/max, merge same bins
+     * 
+     * @param bins
+     * @return
+     */
+    private List<Double> binMerge(List<Double> bins) {
 
-    public List<Double> getUnMergedDataBin(){
+        List<Double> newBins = new ArrayList<Double>();
+        if(bins.size() == 0) {
+            bins.add(Double.NaN);
+            return bins;
+        }
+
+        Double cur = bins.get(0);
+        newBins.add(cur);
+
+        int i = 1;
+        while(i < bins.size()) {
+            if(Math.abs(cur - bins.get(i)) > 1e-10) {
+                newBins.add(bins.get(i));
+            }
+            cur = bins.get(i);
+            i++;
+        }
+
+        if(newBins.size() == 1) {
+            // special case since there is only 1 candidate in the bins
+            double val = newBins.get(0);
+            newBins = Arrays.asList(new Double[] { Double.NEGATIVE_INFINITY, val });
+        } else if(newBins.size() == 2) {
+            newBins.set(0, Double.NEGATIVE_INFINITY);
+        } else {
+            newBins.set(0, Double.NEGATIVE_INFINITY);
+            // remove the max, and became open interval
+            newBins.remove(newBins.size() - 1);
+        }
+        return newBins;
+    }
+
+    @Override
+    public List<Double> getDataBin() {
+        return binMerge(estimator.getQuantiles());
+    }
+
+    public List<Double> getUnMergedDataBin() {
         return estimator.getQuantiles();
     }
 
---
+++ src/main/pig/stats/hadoop2/Stats.pig
@@ -0,0 +1,50 @@
+/**
+ * Copyright [2012-2014] eBay Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+REGISTER '$path_jar'
+
+SET default_parallel $num_parallel;
+SET mapred.map.tasks.speculative.execution true;
+SET mapred.reduce.tasks.speculative.execution true;
+SET mapred.job.queue.name $queue_name;
+SET mapred.task.timeout 1200000;
+SET job.name 'shifu statistic';
+SET io.sort.mb 500
+
+DEFINE IsDataFilterOut  ml.shifu.shifu.udf.PurifyDataUDF('$source_type', '$path_model_config', '$path_column_config');
+DEFINE IsToBinningData  ml.shifu.shifu.udf.FilterBinningDataUDF('$source_type', '$path_model_config', '$path_column_config');
+DEFINE GenBinningData   ml.shifu.shifu.udf.BinningDataUDF('$source_type', '$path_model_config', '$path_column_config');
+DEFINE AddColumnNum     ml.shifu.shifu.udf.AddColumnNumUDF('$source_type', '$path_model_config', '$path_column_config', 'false');
+DEFINE CalculateStats   ml.shifu.shifu.udf.CalculateNewStatsUDF('$source_type', '$path_model_config', '$path_column_config');
+
+-- load and purify data
+data = LOAD '$path_raw_data' USING PigStorage('$delimiter');
+data = FILTER data BY IsDataFilterOut(*);
+
+-- convert data into column based
+data_cols = FOREACH data GENERATE AddColumnNum(*);
+data_cols = FOREACH data_cols GENERATE FLATTEN($0);
+
+-- prepare data and do binning
+data_binning = FILTER data_cols BY IsToBinningData(*);
+data_binning_grp = GROUP data_binning BY $0 PARALLEL $column_parallel;
+binning_info = FOREACH data_binning_grp GENERATE FLATTEN(GenBinningData(*));
+
+-- do stats
+data_stats = GROUP data_cols BY $0;
+data_stats = JOIN data_stats BY $0, binning_info BY columnId;
+
+stats_info = FOREACH data_stats GENERATE FLATTEN(CalculateStats(*));
+STORE stats_info INTO '$path_pre_training_stats' USING PigStorage('|', '-schema');
