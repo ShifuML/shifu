cb4e96ecdd37ef1c038a51cbbee925465ad69ac1
pengshan.zhang@gmail.com <pengzhang@paypal.com>
2015-03-09 10:27:53 +0800

    add auto parallel setting for all pig scripts, no need user to set hadoop parallel number

--- src/main/java/ml/shifu/shifu/core/ConfusionMatrix.java
+++ src/main/java/ml/shifu/shifu/core/ConfusionMatrix.java
@@ -18,7 +18,6 @@ package ml.shifu.shifu.core;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
