98ef352af4c66bf329993d1f493c7309e16013ef
asd7853 <279671687@qq.com>
2015-03-17 15:41:05 +0800

    add ConvergeJudger Test class.

---
+++ src/test/java/ml/shifu/shifu/core/ConvergeJudgerTest.java
@@ -0,0 +1,21 @@
+package ml.shifu.shifu.core;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ConvergeJudgerTest {
+
+    @Test
+    public void testIsConverged() {
+        double train_err = 1.0;
+        double test_err = 3.0;
+        double threshold1 = 1.0;
+        double threshold2 = 2.0;
+        double threshold3 = 3.0;
+        
+        Assert.assertTrue(ConvergeJudger.isConverged(train_err, test_err, threshold1));
+        Assert.assertTrue(ConvergeJudger.isConverged(train_err, test_err, threshold2));
+        Assert.assertFalse(ConvergeJudger.isConverged(train_err, test_err, threshold3));
+        
+    }
+}
