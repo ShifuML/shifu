c0617927d86c37ba7e70fcef380e99182fd71b1a
asd7853 <279671687@qq.com>
2015-03-17 14:38:34 +0800

    change Constants.DEFAULT_THRESHOLD's value to 0.0 . change NNMaster compute's convergence judging process.

--- src/main/java/ml/shifu/shifu/util/Constants.java
+++ src/main/java/ml/shifu/shifu/util/Constants.java
@@ -169,6 +169,6 @@ public interface Constants {
     
     public static final String SHIFU_VARSELECT_SE_OUTPUT_NAME = "se";
 
-    public static final Double DEFAULT_THRESHOLD = Double.valueOf(0.0d);
+    public static final Double DEFAULT_THRESHOLD = Double.valueOf(0.0);
 
 }
