f649878628517dd1a08774db76235e2945f3df85
asd7853 <279671687@qq.com>
2015-03-19 12:48:27 +0800

    Rewrite ConvergeJudger.java

--- src/main/java/ml/shifu/shifu/core/ConvergeJudger.java
+++ src/main/java/ml/shifu/shifu/core/ConvergeJudger.java
@@ -19,25 +19,149 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author xiaobzheng
- * class for training convergence judging.
+ * Class for training convergence judging. 
+ * 
+ * <p> Initialization value for train, test error value is <code>{@link Double#POSITIVE_INFINITY}</code> and
+ * 0.0d for threshold value. So you should set train error, test error and threshold by their setter function 
+ * before any judging operations. After that you can use <code>{@link ConvergeJudger#isConverged()}</code>
+ * to get convergence result. For example,
+ * 
+ * <pre>
+ * ConvergeJudger judger = new ConvergeJudger();
+ * judger.setTrainErr(1.0);
+ * judger.setTestErr(2.0);
+ * judger.setThreshold(0.01);
+ * boolean result = judger.isConverged();
+ * </pre>
+ * 
+ * <p> Use <code>{@link ConvergeJudger#reset()}</code> to restore both errors and threshold to 
+ * their initialization value.
+ * 
+ * <p> You can also get currently input error and threshold setting by using
+ * <code>{@link ConvergeJudger#getJudgeSetting()}</code>.
+ * 
+ * @author xiaobzheng (zheng.xiaobin.roubao@gmail.com)
  */
+
 public class ConvergeJudger {
 
-    private static final Logger log = LoggerFactory.getLogger(ConvergeJudger.class);
+    private Double trainErr = Double.POSITIVE_INFINITY;
+    
+    private Double testErr = Double.POSITIVE_INFINITY;
+    
+    private Double threshold = Double.valueOf(0.0);
+
+    private Double currentAvgErr = Double.POSITIVE_INFINITY;
+    
+    /**
+     * Compute average error using preset training error and testing error. If the average value 
+     * is less than or equal to the preset threshold, it will return true, else false.
+     * 
+     * <p> This operation will upudate current average error.
+     * 
+     * @return convergence judging result.
+     */
+    public boolean isConverged() {
+        CalculateAvgErr();
+
+        return currentAvgErr.compareTo(threshold) <= 0 ? true : false;
+    }
 
     /**
-     * Compute average training error and testing error value and judge if the average value is less than threshold
-     *  
-     * @param train_err training error value
-     * @param test_err test error value
-     * @param threshold 
-     * @return convergence judging result. return true if (train_err + test_err) / 2 <= threshold, else false.
+     * Generate currently judge setting with train error, test error and threshold.
+     * Setting String's format as,
+     * 
+     * <pre>
+     * "[trainErr: 15.026540, testErr: 16.023540, threshold: 0.00001]"
+     * </pre>
+     * 
+     * @return String that presenting currently input setting.
      */
-    public static boolean isConverged(double train_err, double test_err, double threshold) {
-        double avgErr = (train_err + test_err) / 2;
-        log.info("Average err now is: " + avgErr);
-        return avgErr <= threshold ? true : false;
+    public String getJudgeSetting() {
+        return String.format("[trainErr: %f, testErr: %f, threshold: %f]", trainErr, testErr, threshold);
+    }
+    
+    /**
+     * Calculate average value of preset train error and test error. 
+     * 
+     * <p> This operation will upudate current average error.
+     * 
+     * @return (trainErr + testErr) / 2
+     */
+    public Double CalculateAvgErr() {
+        setCurrentAvgErr((trainErr + testErr) / 2);
+        return currentAvgErr;
+    }
+    
+    /**
+     * Reset judger input setting. Set <code>{@link Double#POSITIVE_INFINITY}</code> to train error, test error and 
+     * current average error. Set 0.0d to threshold.
+     */
+    public void reset() {
+        setTrainErr(null);
+        setTestErr(null);
+        setThreshold(null);
+        setCurrentAvgErr(null);
+    }
+    
+    /**
+     * Get average error updated by latest operation: <code>{@link ConvergeJudger#isConverged()}</code>
+     * or <code>{@link ConvergeJudger#CalculateAvgErr()}</code>.
+     * 
+     * <p> Its Initial value is <code>{@link Double#POSITIVE_INFINITY}</code>.
+     * 
+     * @return current average error.
+     */
+    public Double getCurrentAvgErr() {
+        return currentAvgErr;
+    }
+
+    /**
+     * If input error is null, this.currentAvgErr will be assigned <code>{@link Double#POSITIVE_INFINITY}</code>.
+     * 
+     * @param currentAvgErr input average error.
+     */
+    private void setCurrentAvgErr(Double currentAvgErr) {
+        this.currentAvgErr = currentAvgErr == null ? Double.POSITIVE_INFINITY : currentAvgErr;
+    }
+
+    public Double getTrainErr() {
+        return trainErr;
+    }
+
+    /**
+     * If input train err is null, this.trainErr will be assigned <code>{@link Double#POSITIVE_INFINITY}</code>.
+     * 
+     * @param trainErr input train error.
+     */
+    public void setTrainErr(Double trainErr) {
+        this.trainErr = trainErr == null ? Double.POSITIVE_INFINITY : trainErr;
+    }
+
+    public Double getTestErr() {
+        return testErr;
+    }
+
+    /**
+     * If input test err is null, then this.testErr will be assigned <code>{@link Double#POSITIVE_INFINITY}</code>.
+     * 
+     * @param testErr input test error.
+     */
+    public void setTestErr(Double testErr) {
+        this.testErr = testErr == null ? Double.POSITIVE_INFINITY : testErr;
+    }
+
+    public Double getThreshold() {
+        return threshold;
+    }
+
+    /**
+     * If input threshold is null, then this.threshold will be assigned 0.0d.
+     * 
+     * @param threshold input threshold.
+     */
+    public void setThreshold(Double threshold) {
+        this.threshold = threshold == null ? Double.valueOf(0.0) : threshold;
     }
 
 }
