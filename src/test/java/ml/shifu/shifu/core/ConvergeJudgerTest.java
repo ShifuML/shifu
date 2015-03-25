c763ce6d152597312f2b40d40311a85bf156af60
asd7853 <279671687@qq.com>
2015-03-25 11:08:07 +0800

    Use setup to initialize judger instance.

--- src/test/java/ml/shifu/shifu/core/ConvergeJudgerTest.java
+++ src/test/java/ml/shifu/shifu/core/ConvergeJudgerTest.java
@@ -1,17 +1,23 @@
 package ml.shifu.shifu.core;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ConvergeJudgerTest {
 
+    private ConvergeJudger judger;
+
+    @BeforeClass
+    public void setUp() {
+        this.judger = new ConvergeJudger();
+    }
+
     @Test
     public void testJudge() {
-        ConvergeJudger judger = new ConvergeJudger();
-        
         Assert.assertTrue(judger.judge(1.0, 2.0));
         Assert.assertTrue(judger.judge(1.0, 1.0));
         Assert.assertFalse(judger.judge(1.0, 0.1));
     }
-    
+
 }
