42d6e19fc9334dd8495200386903ccc7e7dfb14f
pengshan.zhang@gmail.com <pengzhang@paypal.com>
2015-03-27 14:17:31 +0800

    add new inputformat for varselect se job to combiner small mapper file splits together. By default input split size is 2 * block size

--- src/main/java/ml/shifu/shifu/core/processor/VarSelectModelProcessor.java
+++ src/main/java/ml/shifu/shifu/core/processor/VarSelectModelProcessor.java
@@ -40,6 +40,7 @@ import ml.shifu.shifu.core.dvarsel.wrapper.WrapperMasterConductor;
 import ml.shifu.shifu.core.dvarsel.wrapper.WrapperWorkerConductor;
 import ml.shifu.shifu.core.validator.ModelInspector.ModelStep;
 import ml.shifu.shifu.core.varselect.ColumnInfo;
+import ml.shifu.shifu.core.varselect.VarSelectInputFormat;
 import ml.shifu.shifu.core.varselect.VarSelectMapper;
 import ml.shifu.shifu.core.varselect.VarSelectReducer;
 import ml.shifu.shifu.exception.ShifuErrorCode;
@@ -61,7 +62,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.map.MultithreadedMapper;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
@@ -92,8 +92,6 @@ import com.google.common.base.Splitter;
 public class VarSelectModelProcessor extends BasicModelProcessor implements Processor {
 
     private final static Logger log = LoggerFactory.getLogger(VarSelectModelProcessor.class);
-
-    public static final String SHIFU_DEFAULT_DTRAIN_PARALLEL = "true";
 
     /**
      * Run for the variable selection
@@ -415,7 +413,7 @@ public class VarSelectModelProcessor extends BasicModelProcessor implements Proc
 
         job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(ColumnInfo.class);
-        job.setInputFormatClass(TextInputFormat.class);
+        job.setInputFormatClass(VarSelectInputFormat.class);
         FileInputFormat.setInputPaths(
                 job,
                 ShifuFileUtils.getFileSystemBySourceType(source).makeQualified(
@@ -461,6 +459,8 @@ public class VarSelectModelProcessor extends BasicModelProcessor implements Proc
                         .makeQualified(new Path(super.getPathFinder().getColumnConfigPath(source))).toString());
         conf.set(NNConstants.MAPRED_JOB_QUEUE_NAME, Environment.getProperty(Environment.HADOOP_JOB_QUEUE, "default"));
         conf.set(Constants.SHIFU_MODELSET_SOURCE_TYPE, source.toString());
+        // set mapreduce.job.max.split.locations to 30 to suppress warnings
+        conf.setInt(GuaguaMapReduceConstants.MAPREDUCE_JOB_MAX_SPLIT_LOCATIONS, 30);
 
         Float wrapperRatio = this.modelConfig.getVarSelect().getWrapperRatio();
         if(wrapperRatio == null) {
@@ -529,7 +529,7 @@ public class VarSelectModelProcessor extends BasicModelProcessor implements Proc
         args.add(String.format(NNConstants.MAPREDUCE_PARAM_FORMAT, GuaguaMapReduceConstants.MAPRED_CHILD_JAVA_OPTS,
                 "-Xmn128m -Xms1G -Xmx1G"));
         args.add(String.format(NNConstants.MAPREDUCE_PARAM_FORMAT, GuaguaConstants.GUAGUA_SPLIT_COMBINABLE,
-                Environment.getProperty(GuaguaConstants.GUAGUA_SPLIT_COMBINABLE, SHIFU_DEFAULT_DTRAIN_PARALLEL)));
+                Environment.getProperty(GuaguaConstants.GUAGUA_SPLIT_COMBINABLE, "true")));
         args.add(String.format(NNConstants.MAPREDUCE_PARAM_FORMAT,
                 GuaguaConstants.GUAGUA_SPLIT_MAX_COMBINED_SPLIT_SIZE,
                 Environment.getProperty(GuaguaConstants.GUAGUA_SPLIT_MAX_COMBINED_SPLIT_SIZE, "268435456")));
