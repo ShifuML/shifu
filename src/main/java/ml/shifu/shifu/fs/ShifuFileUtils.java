e7254efd19613fcf894a70be65f04c10fd4f56b6
zhengxiaobin <279671687@qq.com>
2015-04-01 15:35:53 +0800

    remove unused import . Since I use InputStreamReader/OutputStreamReader with explicit encoding instead FileWriter/Reader.

--- src/main/java/ml/shifu/shifu/fs/ShifuFileUtils.java
+++ src/main/java/ml/shifu/shifu/fs/ShifuFileUtils.java
@@ -31,7 +31,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
--- src/test/java/ml/shifu/shifu/util/ShifuCLITest.java
+++ src/test/java/ml/shifu/shifu/util/ShifuCLITest.java
@@ -32,7 +32,6 @@ import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
