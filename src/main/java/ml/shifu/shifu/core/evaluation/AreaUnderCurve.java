7251ce0bfe03d0f291827bae9ebea45f47edba13
zhengxiaobin <279671687@qq.com>
2015-04-22 13:43:20 +0800

    Simplify version of auc calculate.

--- src/main/java/ml/shifu/shifu/core/evaluation/AreaUnderCurve.java
+++ src/main/java/ml/shifu/shifu/core/evaluation/AreaUnderCurve.java
@@ -31,76 +31,86 @@ public class AreaUnderCurve {
     /**
      * Calculate trapezoid area under two points.
      * 
-     * @param first the point with the lower X coordinate.
-     * @param second the point with the higher X coordinate.
+     * @param x1 x of first point.
+     * @param y1 y of first point.
+     * @param x2 x of second point, note: x2 is always considered to be no less than x1, so (x2 - x1) >= 0.
+     * @param y2 y of second point.
      * @return trapezoid area
      */
-    public static double trapezoid(double[] first, double[] second) {
-        return (second[1] + first[1]) * (second[0] - first[0]) / 2.0;
+    public static double trapezoid(double x1, double y1, double x2, double y2) {
+        return (y2 + y1) * (x2 - x1) / 2.0;
     }
     
     
     /**
      * Calculate area of ROC curve based on the PerformanceObject List.
      * 
-     * @param curves PerformanceObject List contains ROC curve data.
+     * @param roc PerformanceObject List contains ROC curve data.
      * @return area under ROC.
      */
     public static double ofRoc(List<PerformanceObject> roc) {
-        return calculate(roc, PerformanceExtractors.getRocPointExtractor());
+        return calculateArea(roc, Performances.getFprExtractor(), Performances.getRecallExtractor());
     }
     
     /**
-     * Calculate area of ROC curve based on the PerformanceObject List.
+     * Calculate area of Weighted ROC curve based on the PerformanceObject List.
      * 
-     * @param curves PerformanceObject List contains ROC curve data.
-     * @return area under ROC.
+     * @param weightedRoc PerformanceObject List contains Weighted ROC curve data.
+     * @return area under Weighted ROC.
      */
     public static double ofWeightedRoc(List<PerformanceObject> weightedRoc) {
-        return calculate(weightedRoc, PerformanceExtractors.getWeightRocPointExtractor());
+        return calculateArea(weightedRoc, Performances.getWeightedFprExtractor(),Performances.getWeightedRecallExtractor());
     }
     
     /**
      * Calculate area of PR curve based on the PerformanceObject List.
      * 
-     * @param curves PerformanceObject List contains PR curve data.
+     * @param pr PerformanceObject List contains PR curve data.
      * @return area under PR.
      */
     public static double ofPr(List<PerformanceObject> pr) {
-        return calculate(pr, PerformanceExtractors.getPrPointExtractor());
+        return calculateArea(pr, Performances.getRecallExtractor(), Performances.getPrecisionExtractor());
     }
     
     /**
-     * Calculate area of PR curve based on the PerformanceObject List.
+     * Calculate area of Weighted PR curve based on the PerformanceObject List.
      * 
-     * @param curves PerformanceObject List contains PR curve data.
-     * @return area under PR.
+     * @param weightedPr PerformanceObject List contains Weighted PR curve data.
+     * @return area under Weighted PR.
      */
     public static double ofWeightedPr(List<PerformanceObject> weightedPr) {
-        return calculate(weightedPr, PerformanceExtractors.getWeightPrPointExtractor());
+        return calculateArea(weightedPr, Performances.getWeightedRecallExtractor(), Performances.getWeightedPrecisionExtractor());
     }
 
     /**
      * Calculate curve area based on the PerformanceObject List and given extractor.
      * 
      * @param perform PerformanceObject List contains curve data.
-     * @param extractor PerformanceExtractor instance used extract desired data from PerformanceObject.
+     * @param xExtractor PerformanceExtractor instance used extract x of point PerformanceObject.
+     * @param yExtractor PerformanceExtractor instance used extract y of point PerformanceObject.
      * @return area under curve.
      */
-    public static double calculate(List<PerformanceObject> perform, PerformanceExtractor<double[]> extractor) {
+    public static double calculateArea(List<PerformanceObject> perform,
+                                       PerformanceExtractor xExtractor, PerformanceExtractor yExtractor) {
         if(perform.size() < 2) {
             throw new IllegalArgumentException("We need at least 2 point to calculate area.");
         }
         
         // accumulate the trapezoid area of every successive two points in the curve.
-        Iterator<PerformanceObject> iter = perform.iterator();
+        Iterator<PerformanceObject> iterator = perform.iterator();
         double sum = 0.0;
-        double[] firstPoint = extractor.extract(iter.next());
-        double[] secondPoint = null;
-        while(iter.hasNext()) {
-            secondPoint = extractor.extract(iter.next());
-            sum += trapezoid(firstPoint, secondPoint);
-            firstPoint = secondPoint;
+        PerformanceObject per = iterator.next();
+        double x1 = xExtractor.extract(per);
+        double y1 = yExtractor.extract(per);
+        double x2;
+        double y2;
+        while(iterator.hasNext()) {
+            per = iterator.next();
+            x2 = xExtractor.extract(per);
+            y2 = yExtractor.extract(per);
+            sum += trapezoid(x1, y1, x2, y2);
+            x1 = x2;
+            y1 = y2;
         }
         
         return sum;
