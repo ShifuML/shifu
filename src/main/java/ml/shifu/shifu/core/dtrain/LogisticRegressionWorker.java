e3f89b0a8615f8d40ea10761e3a200bb52614e83
Zhang <zhang7575@163.com>
2015-06-09 09:37:02 +0800

    add LR support

--- src/main/java/ml/shifu/shifu/core/dtrain/LogisticRegressionWorker.java
+++ src/main/java/ml/shifu/shifu/core/dtrain/LogisticRegressionWorker.java
@@ -18,6 +18,8 @@ package ml.shifu.shifu.core.dtrain;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Properties;
 
 import ml.shifu.guagua.hadoop.io.GuaguaLineRecordReader;
 import ml.shifu.guagua.hadoop.io.GuaguaWritableAdapter;
@@ -26,6 +28,10 @@ import ml.shifu.guagua.util.MemoryDiskList;
 import ml.shifu.guagua.util.NumberFormatUtils;
 import ml.shifu.guagua.worker.AbstractWorkerComputable;
 import ml.shifu.guagua.worker.WorkerContext;
+import ml.shifu.shifu.container.obj.ColumnConfig;
+import ml.shifu.shifu.container.obj.ModelConfig;
+import ml.shifu.shifu.container.obj.RawSourceData.SourceType;
+import ml.shifu.shifu.util.CommonUtils;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -64,6 +70,11 @@ public class LogisticRegressionWorker
      * Output column number
      */
     private int outputNum;
+    
+    /**
+     * Candidate column number
+     */
+    private int candidateNum;
 
     /**
      * In-memory data which located in memory at the first iteration.
@@ -74,11 +85,21 @@ public class LogisticRegressionWorker
      * Local logistic regression model.
      */
     private double[] weights;
+    
+    /**
+     * Model Config read from HDFS
+     */
+    private ModelConfig modelConfig;
+
+    /**
+     * Column Config list read from HDFS
+     */
+    private List<ColumnConfig> columnConfigList;
 
     /**
      * A splitter to split data with specified delimiter.
      */
-    private Splitter splitter = Splitter.on(",");
+    private Splitter splitter = Splitter.on("|");
 
     @Override
     public void initRecordReader(GuaguaFileSplit fileSplit) throws IOException {
@@ -87,9 +108,13 @@ public class LogisticRegressionWorker
 
     @Override
     public void init(WorkerContext<LogisticRegressionParams, LogisticRegressionParams> context) {
-        this.inputNum = NumberFormatUtils.getInt(LogisticRegressionContants.LR_INPUT_NUM,
-                LogisticRegressionContants.LR_INPUT_DEFAULT_NUM);
+        loadConfigFiles(context.getProps());
+        //this.inputNum = NumberFormatUtils.getInt(LogisticRegressionContants.LR_INPUT_NUM,
+          //      LogisticRegressionContants.LR_INPUT_DEFAULT_NUM);
+        int[] inputOutputIndex = NNUtils.getInputOutputCandidateCounts(this.columnConfigList);
+        this.inputNum = inputOutputIndex[0] == 0 ? inputOutputIndex[2] : inputOutputIndex[0];
         this.outputNum = 1;
+        this.candidateNum = inputOutputIndex[2];
         double memoryFraction = Double.valueOf(context.getProps().getProperty("guagua.data.memoryFraction", "0.5"));
         String tmpFolder = context.getProps().getProperty("guagua.data.tmpfolder", "tmp");
         this.dataList = new MemoryDiskList<Data>((long) (Runtime.getRuntime().maxMemory() * memoryFraction), tmpFolder
@@ -109,20 +134,26 @@ public class LogisticRegressionWorker
             return new LogisticRegressionParams();
         } else {
             this.weights = context.getLastMasterResult().getParameters();
-            double[] gradients = new double[this.inputNum + 1];
+            double[] gradients = new double[this.inputNum];
             double finalError = 0.0d;
             int size = 0;
             this.dataList.reOpen();
             for(Data data: dataList) {
-                double error = sigmoid(data.inputs, this.weights) - data.outputs[0];
-                finalError += error * error / 2;
+            	double result = sigmoid(data.inputs, this.weights);
+            	LOG.info("iteration_result:"+result);
+                double error = result - data.outputs[0];
+               // finalError += error * error / 2;
+                finalError += cost(result,data.outputs[0]);
+            	LOG.info("iteration_final:"+finalError);
                 for(int i = 0; i < gradients.length; i++) {
-                    gradients[i] += error * data.inputs[i];
+                    gradients[i] += error * data.inputs[i] * data.significance;
                 }
                 size++;
             }
+        	LOG.info("finish_final:"+finalError);
+        	LOG.info("finish_size:"+size);
             LOG.info("Iteration {} with error {}", context.getCurrentIteration(), finalError / size);
-            return new LogisticRegressionParams(gradients, finalError / size);
+            return new LogisticRegressionParams(gradients, finalError/size);
         }
     }
 
@@ -136,6 +167,15 @@ public class LogisticRegressionWorker
         }
         return 1.0d / (1.0d + Math.exp(-value));
     }
+    
+    public double cost(double result, double output){
+    	if(output==1.0d){
+    		return -Math.log(result);
+    	}
+    	else{
+    		return -Math.log(1-result);
+    	}
+    }
 
     @Override
     protected void postLoad(WorkerContext<LogisticRegressionParams, LogisticRegressionParams> context) {
@@ -146,34 +186,96 @@ public class LogisticRegressionWorker
     public void load(GuaguaWritableAdapter<LongWritable> currentKey, GuaguaWritableAdapter<Text> currentValue,
             WorkerContext<LogisticRegressionParams, LogisticRegressionParams> context) {
         String line = currentValue.getWritable().toString();
-        double[] inputData = new double[inputNum + 1];
+        double[] inputData = new double[inputNum];
         double[] outputData = new double[outputNum];
         int count = 0, inputIndex = 0, outputIndex = 0;
-        inputData[inputIndex++] = 1.0d;
+        long hashcode = 0;
+        double significance = NNConstants.DEFAULT_SIGNIFICANCE_VALUE;
+        //inputData[inputIndex++] = 1.0d;
+        LOG.info("inputnum:"+inputNum);
+        LOG.info("outputnum:"+outputNum);
+        LOG.info("current_line"+line);
+        LOG.info("columnconfig_size:"+this.columnConfigList.size());
         for(String unit: splitter.split(line)) {
-            if(count < inputNum) {
-                inputData[inputIndex++] = Double.valueOf(unit);
-            } else if(count >= inputNum && count < (inputNum + outputNum)) {
-                outputData[outputIndex++] = Double.valueOf(unit);
-            } else {
+            double doubleValue = NumberFormatUtils.getDouble(unit.trim(), 0.0d);
+            if(count == this.columnConfigList.size()) {
+                significance = NumberFormatUtils.getDouble(unit.trim(), NNConstants.DEFAULT_SIGNIFICANCE_VALUE);
                 break;
+            }
+            else{
+                ColumnConfig columnConfig = this.columnConfigList.get(count);
+            if(columnConfig != null && columnConfig.isTarget()) {
+                outputData[outputIndex++] = doubleValue;
+            } else {
+                if(this.inputNum == this.candidateNum) {
+                    // all variables are not set final-selectByFilter
+                    if(CommonUtils.isGoodCandidate(columnConfig)) {
+                        inputData[inputIndex++] = doubleValue;
+                        hashcode = hashcode * 31 + Double.valueOf(doubleValue).hashCode();
+                    }
+                } else {
+                    // final select some variables
+                    if(columnConfig != null && !columnConfig.isMeta() && !columnConfig.isTarget()
+                            && columnConfig.isFinalSelect()) {
+                        inputData[inputIndex++] = doubleValue;
+                        // only fixInitialInput=true, hashcode is effective. Remove Arrays.hashcode to avoid one
+                        // iteration for the input columns. Last weight column should be excluded.
+                        hashcode = hashcode * 31 + Double.valueOf(doubleValue).hashCode();
+                    }
+                }
+            }
             }
             count++;
         }
-        this.dataList.append(new Data(inputData, outputData));
+        this.dataList.append(new Data(inputData, outputData,significance));
+    }
+    
+    private void loadConfigFiles(final Properties props) {
+        try {
+            SourceType sourceType = SourceType.valueOf(props.getProperty(NNConstants.NN_MODELSET_SOURCE_TYPE,
+                    SourceType.HDFS.toString()));
+            this.modelConfig = CommonUtils.loadModelConfig(props.getProperty(NNConstants.SHIFU_NN_MODEL_CONFIG),
+                    sourceType);
+            this.columnConfigList = CommonUtils.loadColumnConfigList(
+                    props.getProperty(NNConstants.SHIFU_NN_COLUMN_CONFIG), sourceType);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static class Data implements Serializable {
 
         private static final long serialVersionUID = 903201066309036170L;
 
-        public Data(double[] inputs, double[] outputs) {
+        public Data(double[] inputs, double[] outputs, double significance) {
             this.inputs = inputs;
             this.outputs = outputs;
+            this.significance = significance;
         }
-
+        private final double significance;
         private final double[] inputs;
         private final double[] outputs;
+    }
+    
+    public void test(String input)
+    {
+    	int count = 0;
+    	for(String unit: splitter.split(input)){
+    		System.out.println("count:"+count);
+    		System.out.println("current_value:"+unit);
+    		count++;
+
+    	}
+    }
+    
+    public static void main(String[] args){
+    	String line ="|1|1.126082|-1.979292|1.307047|1.018816|1.555019|3.408517|2.816882|2.619519|2.274134|2.31";
+    	LogisticRegressionWorker w =  new LogisticRegressionWorker();
+    	//w.test(line);
+    	w.cost(1, 1);
+    	
+    	
     }
 
 }
