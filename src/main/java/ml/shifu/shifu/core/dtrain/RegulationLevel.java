6bb3b1c7ce7cbd04342fd2e75e13348a5fc88a43
pengzhang <pengzhang@paypal.com>
2015-08-05 11:14:07 +0800

    add l1 and l2 for both lr and nn

--- src/main/java/ml/shifu/shifu/core/dtrain/RegulationLevel.java
+++ src/main/java/ml/shifu/shifu/core/dtrain/RegulationLevel.java
@@ -1,5 +1,5 @@
-/**
- * Copyright [2012-2015] PayPal Software Foundation
+/*
+ * Copyright [2013-2015] PayPal Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,23 @@
  */
 package ml.shifu.shifu.core.dtrain;
 
-public interface CommonConstants {
+/**
+ * RegulationLevel for LR and NN models
+ * 
+ * @author Zhang David (pengzhang@paypal.com)
+ */
+public enum RegulationLevel {
+    NONE, L1, L2;
 
-    public static final double DEFAULT_SIGNIFICANCE_VALUE = 1.0d;
-    public static final String MAPREDUCE_PARAM_FORMAT = "-D%s=%s";
-    public static final String GUAGUA_OUTPUT = "guagua.output";
-
-    public static final String LR_REGULARIZED_CONSTANT = "RegularizedConstant";
-
-    public static final String LR_LEARNING_RATE = "LearningRate";
-
-    public static final String REG_LEVEL_KEY = "L1orL2";
+    public static RegulationLevel to(Object obj) {
+        if(obj == null) {
+            return NONE;
+        }
+        for(RegulationLevel rl: RegulationLevel.values()) {
+            if(rl.toString().equalsIgnoreCase(obj.toString())) {
+                return rl;
+            }
+        }
+        return NONE;
+    }
 }
