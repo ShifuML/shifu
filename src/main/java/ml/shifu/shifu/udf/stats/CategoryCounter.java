ba10266d6b637b0a72661b4cbf1f78557cd3b16c
yliu15 <mark.liu@paypal.com>
2016-05-31 16:06:35 +0800

    fix bugs

--- src/main/java/ml/shifu/shifu/udf/stats/CategoryCounter.java
+++ src/main/java/ml/shifu/shifu/udf/stats/CategoryCounter.java
@@ -12,6 +12,7 @@ public class CategoryCounter extends Counter {
 
     private Map<String, Integer> categoryMap = new TreeMap<String, Integer>();
     private List<String> categories;
+    private Integer missCounter;
 
     public CategoryCounter(List<String> categories) {
 
@@ -20,12 +21,20 @@ public class CategoryCounter extends Counter {
         }
 
         this.categories = categories;
+        this.missCounter = 0;
     }
 
     @Override
-    public void addData(String val) {
-        if (categoryMap.containsKey(val)) {
-            categoryMap.put(val, categoryMap.get(val) + 1);
+    public void addData(Object val) {
+        if (val != null) {
+            String sVal = val.toString();
+            if (categoryMap.containsKey(sVal)) {
+                categoryMap.put(sVal, categoryMap.get(sVal) + 1);
+            } else {
+                missCounter++;
+            }
+        } else {
+            missCounter++;
         }
     }
 
@@ -36,6 +45,8 @@ public class CategoryCounter extends Counter {
         for (int i = 0 ; i < categories.size(); i ++) {
             counters.add(categoryMap.get(categories.get(i)));
         }
+
+        counters.add(missCounter);
 
         return counters;
     }
