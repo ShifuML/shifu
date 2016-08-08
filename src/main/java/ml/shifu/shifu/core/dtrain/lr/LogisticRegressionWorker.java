eeb11f709f6c77c584eb858a57a60ca65c0f9123
pengzhang <pengzhang@paypal.com>
2016-08-08 18:07:26 +0800

    add ComputableMonitor to avoid workers not stop issue

--- src/main/java/ml/shifu/shifu/core/dtrain/lr/LogisticRegressionWorker.java
+++ src/main/java/ml/shifu/shifu/core/dtrain/lr/LogisticRegressionWorker.java
@@ -66,7 +66,7 @@ import com.google.common.base.Splitter;
  * <p>
  * L1 and l2 regulations are supported by configuration: RegularizedConstant in model params of ModelConfig.json.
  */
-@ComputableMonitor(timeUnit = TimeUnit.SECONDS, duration = 300)
+@ComputableMonitor(timeUnit = TimeUnit.SECONDS, duration = 240)
 public class LogisticRegressionWorker
         extends
         AbstractWorkerComputable<LogisticRegressionParams, LogisticRegressionParams, GuaguaWritableAdapter<LongWritable>, GuaguaWritableAdapter<Text>> {
