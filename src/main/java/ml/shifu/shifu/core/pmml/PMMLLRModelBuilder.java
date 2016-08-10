f7e06c2d509ab338f251fe242c8b9fe82c9754d5
huzza <super_zhanghaohu@163.com>
2016-08-10 16:45:53 -0700

    enable WOE_ZSCORE support in exporting LR model

--- src/main/java/ml/shifu/shifu/core/pmml/PMMLLRModelBuilder.java
+++ src/main/java/ml/shifu/shifu/core/pmml/PMMLLRModelBuilder.java
@@ -16,14 +16,10 @@
 
 package ml.shifu.shifu.core.pmml;
 
+import java.util.HashMap;
 import java.util.List;
 
-import org.dmg.pmml.DerivedField;
-import org.dmg.pmml.LocalTransformations;
-import org.dmg.pmml.MiningFunctionType;
-import org.dmg.pmml.NumericPredictor;
-import org.dmg.pmml.RegressionNormalizationMethodType;
-import org.dmg.pmml.RegressionTable;
+import org.dmg.pmml.*;
 
 /**
  * The class that converts an LR model to a PMML RegressionModel.
@@ -43,13 +39,46 @@ public class PMMLLRModelBuilder
         table.setIntercept(lr.getBias());
         LocalTransformations lt = pmmlModel.getLocalTransformations();
         List<DerivedField> df = lt.getDerivedFields();
-        for(int i = 0; i<df.size();i++){
-            DerivedField field = df.get(i);
+
+        HashMap<FieldName, FieldName> miningTransformMap = new HashMap<FieldName, FieldName>();
+        for (DerivedField dField : df) {
+            // Apply z-scale normalization on numerical variables
+            if (dField.getExpression() instanceof NormContinuous) {
+                miningTransformMap.put(
+                        ((NormContinuous) dField.getExpression()).getField(),
+                        dField.getName());
+            }
+            // Apply bin map on categorical variables
+            else if (dField.getExpression() instanceof MapValues) {
+                miningTransformMap.put(
+                        ((MapValues) dField.getExpression()).getFieldColumnPairs().get(0).getField(),
+                        dField.getName());
+            }
+            else if (dField.getExpression() instanceof Discretize ) {
+                miningTransformMap.put(
+                        ((Discretize) dField.getExpression()).getField(),
+                        dField.getName());
+            }
+        }
+
+        List<MiningField> miningList = pmmlModel.getMiningSchema().getMiningFields();
+        int index = 0;
+        for (int i = 0; i < miningList.size(); i++) {
+            MiningField mField = miningList.get(i);
+            if (mField.getUsageType() != FieldUsageType.ACTIVE)
+                continue;
+            FieldName mFieldName = mField.getName();
+            FieldName fName = mFieldName;
+            while (miningTransformMap.containsKey(fName)) {
+                fName = miningTransformMap.get(fName);
+            }
+
             NumericPredictor np = new NumericPredictor();
-            np.setName(field.getName());
-            np.setCoefficient(lr.getWeights()[i]);
+            np.setName(fName);
+            np.setCoefficient(lr.getWeights()[index++]);
             table.withNumericPredictors(np);
         }
+
         pmmlModel.withRegressionTables(table);
         return pmmlModel;
     }
