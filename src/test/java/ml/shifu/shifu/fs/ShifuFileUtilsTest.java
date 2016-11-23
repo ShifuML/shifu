5b8b938b7ab79f66645dc409acabbb03bc831c2a
haifwu <haifwu@paypal.com>
2016-11-23 10:16:42 +0800

    fixed UT test (file order doesn't match)

--- src/test/java/ml/shifu/shifu/fs/ShifuFileUtilsTest.java
+++ src/test/java/ml/shifu/shifu/fs/ShifuFileUtilsTest.java
@@ -115,10 +115,7 @@ public class ShifuFileUtilsTest {
 
         filePathList = ShifuFileUtils.expandPath("*-dir/*", SourceType.LOCAL);
         Assert.assertEquals(3, filePathList.size());
-        for(String fileName : filePathList){
-            System.out.println(fileName);
-        }
-        Assert.assertTrue(filePathList.get(0).contains("test-dir/tmp0"));
+        Assert.assertTrue(filePathList.get(0).contains("test-dir/tmp"));
 
         filePathList = ShifuFileUtils.expandPath("~", SourceType.LOCAL);
         Assert.assertEquals(0, filePathList.size());
