b9a0d5679c948cee1afc46a77b536a67aeb9e514
pengzhang <pengzhang@paypal.com>
2017-02-27 11:18:29 +0800

    enable SE by LR

--- src/main/java/ml/shifu/shifu/core/processor/VarSelectModelProcessor.java
+++ src/main/java/ml/shifu/shifu/core/processor/VarSelectModelProcessor.java
@@ -156,9 +156,10 @@ public class VarSelectModelProcessor extends BasicModelProcessor implements Proc
                         selectByFeatureImportance();
                     } else if(filterBy.equalsIgnoreCase(Constants.FILTER_BY_SE)
                             || filterBy.equalsIgnoreCase(Constants.FILTER_BY_ST)) {
-                        if(!Constants.NN.equalsIgnoreCase(modelConfig.getAlgorithm())) {
+                        if(!Constants.NN.equalsIgnoreCase(modelConfig.getAlgorithm())
+                                && !Constants.LR.equalsIgnoreCase(modelConfig.getAlgorithm())) {
                             throw new IllegalArgumentException(
-                                    "Filter by SE/ST only works well in NN. Please check your modelconfig::train.");
+                                    "Filter by SE/ST only works well in NN/LR. Please check your modelconfig::train.");
                         }
                         distributedSEWrapper();
                     } else if(filterBy.equalsIgnoreCase(Constants.FILTER_BY_VOTED)) {
