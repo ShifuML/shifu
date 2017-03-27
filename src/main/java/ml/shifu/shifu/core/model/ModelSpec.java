49d6b58b7c734094ab530b9c074303ca5898b1c1
huzza <super_zhanghaohu@163.com>
2017-03-27 16:00:36 -0700

    1.enable sub-models could have different configuration; 2. fix unit-tests in ConfusionMatrix

--- src/main/java/ml/shifu/shifu/core/model/ModelSpec.java
+++ src/main/java/ml/shifu/shifu/core/model/ModelSpec.java
@@ -1,6 +1,8 @@
 package ml.shifu.shifu.core.model;
 
-import ml.shifu.shifu.container.obj.ModelTrainConf.*;
+import ml.shifu.shifu.container.obj.ColumnConfig;
+import ml.shifu.shifu.container.obj.ModelConfig;
+import ml.shifu.shifu.container.obj.ModelTrainConf.ALGORITHM;
 import org.encog.ml.BasicML;
 
 import java.util.List;
@@ -10,11 +12,18 @@ import java.util.List;
  */
 public class ModelSpec {
     private String modelName;
+    private ModelConfig modelConfig;
+    private List<ColumnConfig> columnConfigList;
     private ALGORITHM algorithm;
     private List<BasicML> models;
 
-    public ModelSpec(String modelName, ALGORITHM algorithm, List<BasicML> models) {
+    public ModelSpec(String modelName,
+                     ModelConfig modelConfig,
+                     List<ColumnConfig> columnConfigList,
+                     ALGORITHM algorithm, List<BasicML> models) {
         this.modelName = modelName;
+        this.modelConfig = modelConfig;
+        this.columnConfigList = columnConfigList;
         this.algorithm = algorithm;
         this.models = models;
     }
@@ -31,4 +40,11 @@ public class ModelSpec {
         return models;
     }
 
+    public ModelConfig getModelConfig() {
+        return modelConfig;
+    }
+
+    public List<ColumnConfig> getColumnConfigList() {
+        return columnConfigList;
+    }
 }
