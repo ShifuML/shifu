542e10fed7e8f7f8385e050904789578f00d383a
pengzhang <pengzhang@paypal.com>
2017-04-17 17:25:49 +0800

    remove log

--- src/main/java/ml/shifu/shifu/core/correlation/CorrelationMultithreadedMapper.java
+++ src/main/java/ml/shifu/shifu/core/correlation/CorrelationMultithreadedMapper.java
@@ -208,7 +208,6 @@ public class CorrelationMultithreadedMapper extends Mapper<LongWritable, Text, I
         // after all sub mapper completed, finalCorrelationMap includes global results and send them to reducer.
         // send to reducer with only one merged copy no matter how many threads
         for(Entry<Integer, CorrelationWritable> entry: finalCorrelationMap.entrySet()) {
-            LOG.info("key {}, value {}.", entry.getKey(), entry.getValue());
             outputKey.set(entry.getKey());
             context.write(outputKey, entry.getValue());
         }
