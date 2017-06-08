fb08d159fa12b7bba617da927f606f8f6d8381c2
pengzhang <pengzhang@paypal.com>
2017-06-08 14:09:02 +0800

    fix a bug on CorrelationMapper.java

--- src/main/java/ml/shifu/shifu/core/correlation/FastCorrelationMapper.java
+++ src/main/java/ml/shifu/shifu/core/correlation/FastCorrelationMapper.java
@@ -21,7 +21,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import ml.shifu.guagua.util.NumberFormatUtils;
 import ml.shifu.shifu.container.obj.ColumnConfig;
