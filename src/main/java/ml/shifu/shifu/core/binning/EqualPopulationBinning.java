0d28b943973d2d347e79c9747f74ba91fcb44012
MiniZhuwei <minizhuwei@gmail.com>
2017-07-05 15:28:55 +0800

    improve time performance of equalpopulation binning to avoid mapred time out

--- src/main/java/ml/shifu/shifu/core/binning/EqualPopulationBinning.java
+++ src/main/java/ml/shifu/shifu/core/binning/EqualPopulationBinning.java
@@ -19,8 +19,8 @@ package ml.shifu.shifu.core.binning;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 import ml.shifu.shifu.core.binning.obj.LinkNode;
 
