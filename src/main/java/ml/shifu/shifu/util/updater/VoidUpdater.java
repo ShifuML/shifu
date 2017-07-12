dc8bc53bbc17653a4a1067a72330f541a70830c8
pengzhang <pengzhang@paypal.com>
2017-07-12 15:15:11 +0800

    format change

--- src/main/java/ml/shifu/shifu/util/updater/VoidUpdater.java
+++ src/main/java/ml/shifu/shifu/util/updater/VoidUpdater.java
@@ -14,6 +14,7 @@ public class VoidUpdater extends BasicUpdater {
         super(modelConfig);
     }
 
+    @Override
     public void updateColumnConfig(ColumnConfig columnConfig) {
         // do nothing
     }
