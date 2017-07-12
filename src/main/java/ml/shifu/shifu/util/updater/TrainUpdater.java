2c58ef7502069a63b2dc1089480f96a76057e801
pengzhang <pengzhang@paypal.com>
2017-07-12 11:15:18 +0800

    fix a bug on varsel: if no column is selected(no finalSelect=true), then filterBy SE, TrainUpdater will set forceSelect to true, then in se job, only forceselected variables will be computed, this is not correct. remove setFinalSelect(true) in TrainUpdater.java

--- src/main/java/ml/shifu/shifu/util/updater/TrainUpdater.java
+++ src/main/java/ml/shifu/shifu/util/updater/TrainUpdater.java
@@ -21,15 +21,20 @@ public class TrainUpdater extends BasicUpdater {
 
         if(this.setMeta.contains(new NSColumn(varName))) {
             columnConfig.setColumnFlag(ColumnConfig.ColumnFlag.Meta);
+            // set to false is OK as if no column are selected, set to false still no one selected
             columnConfig.setFinalSelect(false);
         } else if(this.setForceRemove.contains(new NSColumn(varName))) {
             columnConfig.setColumnFlag(ColumnConfig.ColumnFlag.ForceRemove);
+            // set to false is OK as if no column are selected, set to false still no one selected
             columnConfig.setFinalSelect(false);
         } else if(this.setForceSelect.contains(new NSColumn(varName))) {
-            if ( CollectionUtils.isEmpty(this.setCandidates)
-                    || (CollectionUtils.isNotEmpty(this.setCandidates) && this.setCandidates.contains(new NSColumn(varName))) ) {
+            if(CollectionUtils.isEmpty(this.setCandidates)
+                    || (CollectionUtils.isNotEmpty(this.setCandidates) && this.setCandidates.contains(new NSColumn(
+                            varName)))) {
                 columnConfig.setColumnFlag(ColumnConfig.ColumnFlag.ForceSelect);
-                columnConfig.setFinalSelect(true);
+                // WARN: should not set final select here, imagine user take varsel by SE, the first time is to call
+                // training a model, then forceselected columns will be set to final selected, then all varaibles
+                // selected are only in current final selected columns which is not correct.
             }
         }
     }
