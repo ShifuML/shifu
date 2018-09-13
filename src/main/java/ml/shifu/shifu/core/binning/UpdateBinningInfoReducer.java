9d61435b2ed210ed1ddec7a36d7a0cf735fab072
huzza <super_zhanghaohu@163.com>
2018-09-12 21:53:59 -0700

    check whether it is empty bin category or not

--- src/main/java/ml/shifu/shifu/core/binning/UpdateBinningInfoReducer.java
+++ src/main/java/ml/shifu/shifu/core/binning/UpdateBinningInfoReducer.java
@@ -32,6 +32,7 @@ import ml.shifu.shifu.util.Base64Utils;
 import ml.shifu.shifu.util.CommonUtils;
 import ml.shifu.shifu.util.Constants;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
@@ -263,6 +264,7 @@ public class UpdateBinningInfoReducer extends Reducer<IntWritable, BinningInfoWr
         // To merge categorical binning
         if(columnConfig.isCategorical()
                 && modelConfig.getStats().getCateMaxNumBin() > 0
+                && CollectionUtils.isNotEmpty(columnConfig.getBinCategory())
                 && columnConfig.getBinCategory().size() > modelConfig.getStats().getCateMaxNumBin() ) {
             // only category size large then expected max bin number
             CateBinningStats cateBinningStats = rebinCategoricalValues(
