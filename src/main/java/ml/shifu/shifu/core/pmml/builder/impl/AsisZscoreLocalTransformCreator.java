5668bdc6c4da23417a0412e6f46f8fb68eba36cd
huzza <super_zhanghaohu@163.com>
2019-04-04 15:37:45 -0700

    support ASIS normalization method in Shifu, when exporting PMML

---
+++ src/main/java/ml/shifu/shifu/core/pmml/builder/impl/AsisZscoreLocalTransformCreator.java
@@ -0,0 +1,52 @@
+package ml.shifu.shifu.core.pmml.builder.impl;
+
+import ml.shifu.shifu.container.obj.ColumnConfig;
+import ml.shifu.shifu.container.obj.ModelConfig;
+import ml.shifu.shifu.container.obj.ModelNormalizeConf;
+import ml.shifu.shifu.util.NormalUtils;
+import org.dmg.pmml.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright [2013-2018] PayPal Software Foundation
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+public class AsisZscoreLocalTransformCreator extends ZscoreLocalTransformCreator {
+
+    public AsisZscoreLocalTransformCreator(ModelConfig modelConfig, List<ColumnConfig> columnConfigList) {
+        super(modelConfig, columnConfigList);
+    }
+
+    public AsisZscoreLocalTransformCreator(ModelConfig modelConfig, List<ColumnConfig> columnConfigList, boolean isConcise) {
+        super(modelConfig, columnConfigList, isConcise);
+    }
+
+    /**
+     * Create @DerivedField for numerical variable
+     *
+     * @param config
+     *            - ColumnConfig for numerical variable
+     * @param cutoff
+     *            - cutoff of normalization
+     * @param normType
+     *            - the normalization method that is used to generate DerivedField
+     * @return DerivedField for variable
+     */
+    protected List<DerivedField> createNumericalDerivedField(ColumnConfig config, double cutoff,
+            ModelNormalizeConf.NormType normType) {
+        return new ArrayList<DerivedField>();
+    }
+}
